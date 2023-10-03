47aa584097f665b2ae5c98415782369b86f0f7a5
N78braga <nandobraga78@gmail.com>
2023-10-03 18:17:01 -0300

    Initial commit

---
+++ src/resolucaoDoDesafioDaDIO/desafio01/Desafio01.java
@@ -0,0 +1,37 @@
+package resolucaoDoDesafioDaDIO.desafio01;
+
+import java.util.Scanner;
+
+public class Desafio01 {
+ /*   public static void main(String[] args) {
+        // Lendo os dados de Entrada:
+        Scanner scanner = new Scanner(System.in);
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); // Consome a quebra de linha após a entrada do número da conta
+        String nomeTitular = scanner.nextLine();
+        double saldo = scanner.nextDouble();
+
+        // Criar uma instância de "ContaBancaria" com os valores de Entrada.
+        resolucaoDoDesafioDaDIO.desafio02.ContaBancaria conta = new resolucaoDoDesafioDaDIO.desafio02.ContaBancaria(numeroConta, nomeTitular, saldo);
+
+        System.out.println("Informacoes:");
+        // Imprimir as informações da conta usando o objeto criado acima.
+        System.out.println("Conta: " + conta.numero);
+        System.out.println("Titular: " + conta.titular);
+        System.out.printf("Saldo: R$ %.1f%n", conta.saldo);
+    }
+}
+
+class ContaBancaria {
+    int numero;
+    String titular;
+    double saldo;
+
+    public ContaBancaria(int numero, String titular, double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }*/
+}
+
+
